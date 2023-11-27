010a7f003a6a0d94227e8f83c4434af97c3862f7
unknown <ulfaidi2002@gmail.com>
2023-11-27 23:37:23 +0700

    main

--- resources/views/Page/Dashboard/show.blade.php
+++ resources/views/Page/Dashboard/show.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.main')
 @section('content')
-<div class="main-panel">
-    <div class="content-wrapper">
-<h1>test ini</h1>
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <h1>test ini add</h1>
+        </div>
     </div>
-</div>
-
-
 @endsection
